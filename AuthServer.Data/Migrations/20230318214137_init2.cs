cdaed1c416c1846fc8b9c00b3b057d8124bd11d8
smtdeveloper <sametakcatoki@gmail.com>
2023-05-21 18:27:59 +0300

    mig-init

---
+++ AuthServer.Data/Migrations/20230318214137_init2.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthServer.Data.Migrations
+{
+    public partial class init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Produts",
+                table: "Produts");
+
+            migrationBuilder.RenameTable(
+                name: "Produts",
+                newName: "Products");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "Produts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Produts",
+                table: "Produts",
+                column: "Id");
+        }
+    }
+}
